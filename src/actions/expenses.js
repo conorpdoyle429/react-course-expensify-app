3dc646b0f644dc90c526095d9b99b3b9e3177bc0
Conor Doyle <conorpdoyle429@gmail.com>
2020-07-30 13:46:55 +0100

    Get firebase expenses on start

--- src/actions/expenses.js
+++ src/actions/expenses.js
@@ -43,3 +43,30 @@ export const editExpense = (id, updates) => ({
     id,
     updates
 }); 
+
+// SET_EXPENSES
+export const setExpenses = (expenses) => ({
+    type: 'SET_EXPENSES',
+    expenses
+});
+
+export const startSetExpenses = () => {
+    // fetch all expense data
+
+    // parse data into an array
+
+    // dispatch SET_EXPENSES
+    return (dispatch) => {
+        return database.ref('expenses').once('value').then((snapshot) => {
+            const expenses = [];
+            snapshot.forEach((expense) => {
+                expenses.push({
+                    id: expense.key,
+                    ...expense.val()
+                });
+            });
+            console.log(expenses);
+            dispatch(setExpenses(expenses));
+        });
+    }
+};
