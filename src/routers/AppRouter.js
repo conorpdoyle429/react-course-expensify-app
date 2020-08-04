b1f34db14077c777a8a5addc7c1f6db6a4eed42e
Conor Doyle <conorpdoyle429@gmail.com>
2020-08-04 14:49:42 +0100

    Store user data in user area

--- src/routers/AppRouter.js
+++ src/routers/AppRouter.js
@@ -4,10 +4,10 @@ import createHistory from 'history/createBrowserHistory';
 import ExpenseDashboard from '../components/ExpenseDashboard';
 import AddExpense from '../components/AddExpense';
 import EditExpense from '../components/EditExpense';
-import Help from '../components/Help';
 import NotFound from '../components/NotFound';
 import Login from '../components/Login';
 import PrivateRoute from './PrivateRoute';
+import PublicRoute from './PublicRoute';
 
 export const history = createHistory();
 
@@ -15,11 +15,10 @@ const AppRouter = () => (
     <Router history={history}>
         <div>
             <Switch>
-                <Route path="/" component={Login} exact={true} />
+                <PublicRoute path="/" component={Login} exact={true} />
                 <PrivateRoute path="/dashboard" component={ExpenseDashboard} />
                 <PrivateRoute path="/create" component={AddExpense} />
                 <PrivateRoute path="/edit/:id" component={EditExpense} />
-                <Route path="/help" component={Help} />
                 <Route component={NotFound} />
             </Switch>
         </div>
--- src/tests/components/EditExpense.test.js
+++ src/tests/components/EditExpense.test.js
@@ -27,12 +27,12 @@ test('should render EditExpense', () => {
 
 test('should handle editExpense', () => {
     wrapper.find('ExpenseForm').prop('onSubmit')(expenses[1]);
-    expect(history.push).toHaveBeenLastCalledWith('/');
+    expect(history.push).toHaveBeenLastCalledWith('/dashboard');
     expect(startEditExpense).toHaveBeenLastCalledWith(expenses[1].id, expenses[1]);
 });
 
 test('should handle removeExpense', () => {
     wrapper.find('button').simulate('click');
-    expect(history.push).toHaveBeenLastCalledWith('/');
+    expect(history.push).toHaveBeenLastCalledWith('/dashboard');
     expect(startRemoveExpense).toHaveBeenLastCalledWith({id: expenses[1].id});
 });
